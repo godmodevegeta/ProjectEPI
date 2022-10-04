96412abdea68022a56d652453e9c6247ba97aa50
godmodenorfin <kshubham1904@gmail.com>
2022-10-04 13:31:52 +0530

    some changes

--- src/TruffleWithArrays/RotateArray.java
+++ src/TruffleWithArrays/RotateArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RotateArray {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
-        int k = 5;
+        int k = 1;
         System.out.println(Arrays.toString(solution(arr, k)));
 
     }
