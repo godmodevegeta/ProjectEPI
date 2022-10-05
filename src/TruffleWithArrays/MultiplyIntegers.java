1f356b8be8d65b3ec1c2233eb77a970a6a4de1ff
godmodenorfin <kshubham1904@gmail.com>
2022-10-05 23:13:39 +0530

    Created MultiplyIntegers

---
+++ src/TruffleWithArrays/MultiplyIntegers.java
@@ -0,0 +1,11 @@
+package TruffleWithArrays;
+
+public class MultiplyIntegers {
+    public static void main(String[] args) {
+        int[] nums1 = { 5, 2, 8, 6};
+        int[] nums2 = { 3, 5, 3};
+
+        
+
+    }
+}
